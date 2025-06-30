2aecac089c78cf653d7acad8640ffc0c6e22a651
Roman Steger <roman.steger11@gmail.com>
2025-06-30 20:56:03 +0200

    chore(PlatformIO): PlatformIO zum Projekt hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
-void setup() {
-  // Setup Code
+#include <Arduino.h>
 
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // Main Code
+  // put your main code here, to run repeatedly:
+}
 
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
