c424e7b10e5ea8c2a9b2a5945cee90bd32fce0b1
Roman Steger <roman.steger11@gmail.com>
2025-07-07 23:44:02 +0200

    refactor(NTPTimeSync): NTPTimeSync in .h und .cpp aufteilen aufgrund von Kompilierfehler im Zusammenhang mit Logger

---
+++ src/webservice/NTPTimeSync.cpp
@@ -0,0 +1,61 @@
+#include "NTPTimeSync.h" // Inkludiere die eigene Header-Datei
+#include "../logger/Logger.h" // <-- HIER MUSS Logger.h VOLLSTÄNDIG INKLUDIERT WERDEN,
+                              // da Logger::log tatsächlich aufgerufen wird und der Compiler
+                              // die vollständige Definition von Logger benötigt.
+
+// Implementierung der begin() Methode
+bool NTPTimeSync::begin() {
+  // Überprüfen, ob die Konfigurationsdaten gesetzt sind
+  if (_ssid == nullptr || _password == nullptr || _ntpServer == nullptr || _updateInterval == 0) {
+      Logger::log(LogLevel::Error, "NTPTimeSync::begin() aufgerufen ohne vollständige Konfiguration. Bitte getInstance mit allen Parametern aufrufen.");
+      return false;
+  }
+
+  Logger::log(LogLevel::Info, "Verbinde mit WLAN...");
+  WiFi.begin(_ssid, _password);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+    delay(500);
+    Logger::log(LogLevel::Info, ".");
+    attempts++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Logger::log(LogLevel::Info, "Verbunden mit WLAN!");
+    Logger::log(LogLevel::Info, "IP-Adresse: " + WiFi.localIP().toString());
+
+    _timeClient.begin();
+    Logger::log(LogLevel::Info, "Synchronisiere Zeit mit NTP...");
+    while(!_timeClient.update()) {
+      delay(500);
+      Logger::log(LogLevel::Info, ".");
+    }
+    Logger::log(LogLevel::Info, "Zeit synchronisiert.");
+    return true;
+  } else {
+    Logger::log(LogLevel::Error, "Verbindung fehlgeschlagen! Bitte WLAN-Daten prüfen.");
+    return false;
+  }
+}
+
+// Implementierung der update() Methode
+void NTPTimeSync::update() {
+  _timeClient.update();
+}
+
+// Implementierung der getFormattedTime() Methode
+String NTPTimeSync::getFormattedTime() {
+  return _timeClient.getFormattedTime();
+}
+
+// Implementierung der getEpochTime() Methode
+time_t NTPTimeSync::getEpochTime() {
+  return _timeClient.getEpochTime();
+}
+
+// Implementierung des privaten Konstruktors
+NTPTimeSync::NTPTimeSync(const char* ssid, const char* password, const char* ntpServer, long timeOffset, long updateInterval)
+  : _ssid(ssid), _password(password), _ntpServer(ntpServer), _timeOffset(timeOffset), _updateInterval(updateInterval),
+    _timeClient(_ntpUDP, ntpServer, timeOffset, updateInterval) {
+  // Der Konstruktor kann leer bleiben, da alle Member in der Initialisierungsliste gesetzt werden.
+}
--- src/webservice/NTPTimeSync.h
+++ src/webservice/NTPTimeSync.h
@@ -5,17 +5,22 @@
 #include <WiFiS3.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
-#include "../logger/Logger.h"
-#include "../logger/LogLevel.h"
+#include "../logger/LogLevel.h" // LogLevel.h muss hier bleiben, da es im Public-Interface verwendet wird
+
+// --- FORWARD DECLARATION für die Logger-Klasse ---
+// Da NTPTimeSync in seiner Deklaration nur Logger::log aufruft (statische Methode),
+// aber kein Logger-Objekt als Member hat, reicht eine Forward Declaration.
+// Die vollständige Definition von Logger wird erst in der .cpp-Datei benötigt.
+class Logger;
 
 class NTPTimeSync {
 public:
   // Statische Methode, um die einzige Instanz von NTPTimeSync zu erhalten.
   // Die Konfigurationsparameter werden hier beim ERSTEN Aufruf übergeben.
+  // WICHTIG: Die statische 'instance' wird hier im Header definiert,
+  // da sie in einer Inline-Funktion (getInstance) verwendet wird.
   static NTPTimeSync& getInstance(const char* ssid = nullptr, const char* password = nullptr,
                                   const char* ntpServer = nullptr, long timeOffset = 0, long updateInterval = 0) {
-    // Statische Variable, die die einzige Instanz hält.
-    // Sie wird nur einmal initialisiert, wenn getInstance zum ersten Mal aufgerufen wird.
     static NTPTimeSync instance(ssid, password, ntpServer, timeOffset, updateInterval);
     return instance;
   }
@@ -23,62 +28,21 @@ public:
   // Methode zur Initialisierung der WLAN-Verbindung und NTP
   // Return: true  = Verbindung konnte aufgebaut werden
   //         false = Verbindungsaufbau fehlgeschlagen
-  bool begin() {
-    // Überprüfen, ob die Konfigurationsdaten gesetzt sind
-    if (_ssid == nullptr || _password == nullptr || _ntpServer == nullptr || _updateInterval == 0) {
-        Logger::log(LogLevel::Error, "NTPTimeSync::begin() aufgerufen ohne vollständige Konfiguration. Bitte getInstance mit allen Parametern aufrufen.");
-        return false;
-    }
-
-    Logger::log(LogLevel::Info, "Verbinde mit WLAN...");
-    WiFi.begin(_ssid, _password);
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20) {
-      delay(500);
-      Logger::log(LogLevel::Info, ".");
-      attempts++;
-    }
-
-    if (WiFi.status() == WL_CONNECTED) {
-      Logger::log(LogLevel::Info, "Verbunden mit WLAN!");
-      Logger::log(LogLevel::Info, "IP-Adresse: " + WiFi.localIP().toString());
-
-      _timeClient.begin();
-      Logger::log(LogLevel::Info, "Synchronisiere Zeit mit NTP...");
-      while(!_timeClient.update()) {
-        delay(500);
-        Logger::log(LogLevel::Info, ".");
-      }
-      Logger::log(LogLevel::Info, "Zeit synchronisiert.");
-      return true;
-    } else {
-      Logger::log(LogLevel::Error, "Verbindung fehlgeschlagen! Bitte WLAN-Daten prüfen.");
-      return false;
-    }
-  }
+  bool begin(); // Nur Deklaration
 
   // Methode, die regelmäßig aufgerufen werden sollte, um die Zeit zu aktualisieren
-  void update() {
-    _timeClient.update();
-  }
+  void update(); // Nur Deklaration
 
   // Gibt die formatierte Uhrzeit (HH:MM:SS) zurück
-  String getFormattedTime() {
-    return _timeClient.getFormattedTime();
-  }
+  String getFormattedTime(); // Nur Deklaration
 
   // Gibt den Unix-Timestamp (Sekunden seit 1. Januar 1970) zurück
-  time_t getEpochTime() {
-    return _timeClient.getEpochTime();
-  }
+  time_t getEpochTime(); // Nur Deklaration
 
 private:
   // Privater Konstruktor, um direkte Instanziierung zu verhindern
   // Wird nur von getInstance() aufgerufen.
-  NTPTimeSync(const char* ssid, const char* password, const char* ntpServer, long timeOffset, long updateInterval)
-    : _ssid(ssid), _password(password), _ntpServer(ntpServer), _timeOffset(timeOffset), _updateInterval(updateInterval),
-      _timeClient(_ntpUDP, ntpServer, timeOffset, updateInterval) {
-  }
+  NTPTimeSync(const char* ssid, const char* password, const char* ntpServer, long timeOffset, long updateInterval);
 
   // Private Kopierkonstruktor und Zuweisungsoperator verhindern Kopien
   NTPTimeSync(const NTPTimeSync&) = delete;
