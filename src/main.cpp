de8ba552e5238d0884d43aa87bbf4aa4b3ffb92a
Roman Steger <roman.steger11@gmail.com>
2025-07-09 20:54:58 +0200

    fix(main): Zeitinstanz an Logger übergeben, um bei der Log ausgabe einen Zeitstempel auszugeben.

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,21 @@ const char* ntpServer = "ntp.metas.ch";
 const long timeOffset = 2 * 3600; // MESZ (Sommerzeit) = +2 Stunden | MEZ (Winterzeit) = +1 Stunde 
 const long updateInterval = 60000; // 1 Minute
 
+// Logger einstellungen
+const LogLevel logLevel = LogLevel::Info;
+
 void setup() {
   // Logger Initialisieren
-  Logger::setup(LogLevel::Info);
+  Logger::setup(logLevel);
   Logger::log(LogLevel::Info, "Programm gestartet!");
 
   // NTPTimeSync Initialiseren
   if (NTPTimeSync::getInstance(SECRET_SSID, SECRET_PASSWORD, ntpServer, timeOffset, updateInterval).begin()) {
+    // Zeit an Logger weiter geben
+    Logger::setup(logLevel, &NTPTimeSync::getInstance());
+
     Logger::log(LogLevel::Info, "NTP-Synchronisation erfolgreich abgeschlossen.");
+
   } else {
     Logger::log(LogLevel::Error, "NTP-Synchronisation fehlgeschlagen!");
   }
