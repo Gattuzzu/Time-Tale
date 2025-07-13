cd5745ca7a5f074b192671427c6635fa39c6ee47
Roman Steger <roman.steger11@gmail.com>
2025-07-13 12:33:29 +0200

    feat(Settings.h): Umbau der Einstellungen von main.cpp in Settings.h

---
+++ src/Settings.h
@@ -0,0 +1,12 @@
+// NTP Konfiguration
+#define NTP_SERVER "ntp.metas.ch"
+#define TIME_OFFSET 7200 // MESZ (Sommerzeit) = +2 Stunden | MEZ (Winterzeit) = +1 Stunde 
+#define UPDATE_INTERVALL 60000 // 1 Minute
+
+// Logger Konfiguration
+#define LOG_LEVEL LogLevel::Info
+
+// API Konfiguration
+#define WEATHER_API_SERVER "weather.googleapis.com"
+#define LATITUDE 46.774
+#define LONGITUDE 7.640
--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,20 @@
 #include "webservice/api/weather/WeatherClient.h"
 #include "webservice/api/weather/WeatherData.h"
 #include "Secrets.h"
-
-// NTP Konfiguration
-const char* ntpServer = "ntp.metas.ch";
-const long timeOffset = 2 * 3600; // MESZ (Sommerzeit) = +2 Stunden | MEZ (Winterzeit) = +1 Stunde 
-const long updateInterval = 60000; // 1 Minute
-
-// Logger Konfiguration
-const LogLevel logLevel = LogLevel::Info;
-
-// API Konfiguration
-const char* weatherApiServer = "weather.googleapis.com";
-const float latitude = 46.774;
-const float longitude = 7.640;
-
+#include "Settings.h"
 
 // Lokale Speicher
 WeatherData currentWeatherData;
 
 void setup() {
   // Logger initialisieren
-  Logger::setup(logLevel);
+  Logger::setup(LOG_LEVEL);
   Logger::log(LogLevel::Info, "Programm gestartet!");
 
   // NTPTimeSync initialiseren
-  if (NTPTimeSync::getInstance(SECRET_SSID, SECRET_PASSWORD, ntpServer, timeOffset, updateInterval).begin()) {
+  if (NTPTimeSync::getInstance(SECRET_SSID, SECRET_PASSWORD, NTP_SERVER, TIME_OFFSET, UPDATE_INTERVALL).begin()) {
     // Zeit an Logger weiter geben
-    Logger::setup(logLevel, &NTPTimeSync::getInstance());
+    Logger::setup(LOG_LEVEL, &NTPTimeSync::getInstance());
 
     Logger::log(LogLevel::Info, "NTP-Synchronisation erfolgreich abgeschlossen.");
 
@@ -40,7 +27,7 @@ void setup() {
   }
 
   // Weather API initialisieren
-  WeatherClient::getInstance(weatherApiServer, GOOGLE_ACCESS_TOKEN);
+  WeatherClient::getInstance(WEATHER_API_SERVER, GOOGLE_ACCESS_TOKEN);
 
 }
 
@@ -48,7 +35,7 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   // Wetter Daten abfragen
-  if (WeatherClient::getInstance().getCurrentConditions(latitude, longitude, currentWeatherData)) {
+  if (WeatherClient::getInstance().getCurrentConditions(LATITUDE, LONGITUDE, currentWeatherData)) {
     Logger::log(LogLevel::Info, "Wetterdaten erfolgreich abgerufen.");
     // currentWeatherData.printToSerial();
   } else {
